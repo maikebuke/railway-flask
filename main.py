c2a21f363c2533a6f6ed2d104ee39db726f47f8a
litiantian <litiantian@ustc.edu>
2023-05-12 16:47:09 +0800

    test

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 import os
 import sys
+import requests
 from datetime import datetime
 
 app = Flask(__name__)
@@ -8,9 +9,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": f"Welcome to your Flask app, python version: {sys.version}",
+    return jsonify({"Choo Choo": f"Welcome to your Flask app", 
+                    "python":  f"{sys.version}",
                     "time": str(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))})
 
+
+@app.route("test")
+def test():
+    resp = requests.get("https://api.openai.com/v1/chat/completions")
+    return resp
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
