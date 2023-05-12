fc1266ee638f43f82fd58a3d8412ede69088dc31
litiantian <litiantian@ustc.edu>
2023-05-12 16:39:32 +0800

    添加当前时间

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from flask import Flask, jsonify
 import os
 import sys
+from datetime import datetime
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": f"Welcome to your Flask app, python version: {sys.version}"})
+    return jsonify({"Choo Choo": f"Welcome to your Flask app, python version: {sys.version}",
+                    "time": str(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))})
 
 
 if __name__ == '__main__':
